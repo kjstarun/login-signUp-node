62e010fe9f79d8cc1f09fda1dfe1b98804c730a9
kjstarun <kjstarun@gmail.com>
2023-04-05 15:04:56 +0530

    updated

--- src/api/users/users.model.js
+++ src/api/users/users.model.js
@@ -2,8 +2,18 @@ import Mongoose from "mongoose";
 
 const UserSchema = new Mongoose.Schema(
   {
-    userName: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: /^(?!.*\s)[^\s]+$/,
+    },
+    password: {
+      type: String,
+      required: true,
+      validate:
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{9,29}$/,
+    },
   },
   { timestamps: true }
 );
